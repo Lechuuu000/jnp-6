aa6db24919e1ed6378ef296740803570a2a1c340
Leszek Troc <lt418485@students.mimuw.edu.pl>
2021-01-20 01:43:39 +0100

    Initial commit

---
+++ computer.h
@@ -0,0 +1,11 @@
+#ifndef JNP_6_COMPUTER_H
+#define JNP_6_COMPUTER_H
+
+#include <iostream>
+#include "ooasm.h"
+
+class Computer {
+
+};
+
+#endif // JNP_6_COMPUTER_H
---
+++ ooasm.h
@@ -0,0 +1,8 @@
+#ifndef JNP_6_OOASM_H
+#define JNP_6_OOASM_H
+
+class OOAsmProgram {
+
+};
+
+#endif // JNP_6_OOASM_H
